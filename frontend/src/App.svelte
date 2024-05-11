19ce30cb7bc6472f46ff969d8a4b80fc58f5bf56
lnzt <prnis360@gmail.com>
2024-05-11 22:55:49 +0800

    added client side routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import {Router, Link, Route} from "svelte-routing";
+  import LoginPage from "./lib/pages/LoginPage.svelte";
+  import TimetableViewPage from "./lib/pages/TimetableViewPage.svelte";
+  import RegisterPage from "./lib/pages/RegisterPage.svelte";
+  import AboutPage from "./lib/pages/AboutPage.svelte";
+
 
 </script>
+<Router>
+  <Route path="/login" component={LoginPage}/>
+  <Route path="/timetable" component={TimetableViewPage}/>
+  <Route path="/register" component={RegisterPage}/>
+  <Route path="/about" component={AboutPage}/>
+</Router>
 
 <main>
-  <div>
-
-  </div>
-
 </main>
 
 <style>
