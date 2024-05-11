cafa757bc137c22e8a75fefe9f33b606c5fb13fd
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-11 22:19:00 +0800

    Update main.rs
    Added Dotenv and some documentation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,19 @@
 pub mod routes;
 use routes::auth::login;
 use routes::auth::register;
+use dotenv
 
 #[async_std::main]
 async fn main() -> tide::Result<()>{
+    // create app
     let mut app = tide::new();
+
+    // setup routes
     app.at("/login").post(login);
     app.at("/register").post(register);
+
+    // attach to IP and port
     app.listen("127.0.0.1:8080").await?;
+
     Ok(())
 }
