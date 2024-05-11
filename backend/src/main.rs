84753826a07f3d6dc730278bf23fe59fe7e8179a
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-11 22:28:24 +0800

    Update main.rs
    listen to ip_address and port created by .env file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,15 @@
 pub mod routes;
+use std::env;
+
 use routes::auth::login;
 use routes::auth::register;
-use dotenv
+use dotenv::dotenv;
 
 #[async_std::main]
 async fn main() -> tide::Result<()>{
+    // load dotenv
+    dotenv().expect("No .env file found");
+
     // create app
     let mut app = tide::new();
 
@@ -13,7 +18,10 @@ async fn main() -> tide::Result<()>{
     app.at("/register").post(register);
 
     // attach to IP and port
-    app.listen("127.0.0.1:8080").await?;
+    let ip_address = env::var("IP_ADDRESS").expect("Ip address needs to be set in .env");
+    let port = env::var("PORT").expect("Port needs to be set in .env");
+    let ip_port = ip_address.clone() + ":" + &port;
+    app.listen(ip_port).await?;
 
     Ok(())
 }
