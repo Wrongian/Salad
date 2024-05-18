73f2437ff94f1313720ee8f703f84c2c6116bc30
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-18 23:17:12 +0800

    Update 00001_users_table.sql
    sql statement for creating simple table

---
+++ backend/migrations/00001_users_table.sql
@@ -0,0 +1,9 @@
+-- Simple version for testing  
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+    bio VARCHAR
+    is_private BOOLEAN NOT NULL
+);
