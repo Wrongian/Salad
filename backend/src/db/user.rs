6faee6e738ad3afd4f839ad81f912e15158f385e
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-19 00:29:54 +0800

    Update user.rs
    Create function to add a new user

--- backend/src/db/user.rs
+++ backend/src/db/user.rs
@@ -3,5 +3,19 @@ struct User {
     password: String,  
     email: String,
     bio: String,
-    is_private: String
+    is_private: bool
+}
+
+async fn create(user: &User, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "INSERT INTO users (username, password, email, bio, is_private) VALUES ($1, $2, $3)";
+
+    sqlx::query(query)
+        .bind(&user.username)
+        .bind(&user.password)
+        .bind(&user.email)
+        .bind(&user.bio)
+        .bind(&user.is_private)
+        .execute(pool)
+        .await?;
+    Ok(());
 }
