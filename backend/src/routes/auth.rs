3301c0aeb13ec718d514debe0c8ca9a2e57a7db4
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-20 01:43:28 +0800

    Update auth.rs
    made new user object with email being a placeholder

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -7,11 +7,13 @@ use scrypt::{
     Scrypt
 };
 use tide::prelude::*;
+use crate::db::user::create;
+use crate::db::user::User;
 
 #[derive(Debug, Deserialize)]
-struct LoginParams {
-    username : String,
-    password : String,
+pub struct LoginParams {
+    pub username : String,
+    pub password : String,
 }
 
 pub async fn login(mut req: Request<()>) -> tide::Result {
@@ -23,8 +25,29 @@ pub async fn login(mut req: Request<()>) -> tide::Result {
 
 pub async fn register(mut req: Request<()>) -> tide::Result {
     // probably will change later to form
-    // todo
+    // email will be a placeholder for now
     let LoginParams {username, password} = req.body_json().await?;
+    // generate salt
+    let salt: SaltString = generate_salt();
+    // hash salt
+    let hashed_password = hash_password(&password, &salt);
+    // get SaltString as String
+    let salt_str = salt.to_string();
+    // get back the SaltString
+    // let salt = SaltString::from_b64(salt_str);
+    
+    // create new user
+    let new_user = User {
+        username: username.clone(),
+        password: hashed_password,
+        email: "bruh@gmail.com".to_string(),
+        is_private: false,
+        bio: None,
+        salt: salt_str,
+    };
+
+    // create(&user, &pool);
+
     Ok(format!("Username: {}\n Password: {}", username, password).into())
 }
 
