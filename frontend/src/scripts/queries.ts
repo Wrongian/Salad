f72e3ade3ec8a70b3e4a8eaf0f40d713fdbe4f21
Linxin <prnis360@gmail.com>
2024-05-20 23:35:18 +0800

    added bearer store and basic profile update query

--- frontend/src/scripts/queries.ts
+++ frontend/src/scripts/queries.ts
@@ -1,3 +1,6 @@
+import type { TUpdateProfileQuery } from "./query.d.ts";
+import { authStore } from "../stores/stores.js";
+import { get } from "svelte/store";
 const SERVER_IP_ADDR = import.meta.env.VITE_BACKEND_IP_ADDR
 
 export const login = async (username: string, password: string): Promise<void> => {
@@ -24,5 +27,25 @@ export const register = async (email: string, username: string, password: string
             password: password
         })
     });
+
+}
+
+export const resetPassword = async (username: string) => {
+    // TODO:
+    const response = await fetch(`${SERVER_IP_ADDR}/users`, {
+        method: "PUT",
+        mode: "no-cors",
+    });
+}
+
+export const updateProfile = async (updateQuery: TUpdateProfileQuery) => {
+    const response = await fetch(`${SERVER_IP_ADDR}/profiles`, {
+        method: "PUT",
+        mode: "no-cors",
+        headers: {
+            'Authorization': `Bearer ${get(authStore)}`
+        },
+        body: JSON.stringify(updateQuery)
+    })
 
 }
