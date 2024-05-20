534a97552b7baab998756b61ff75360218a0158e
Linxin <prnis360@gmail.com>
2024-05-20 22:47:02 +0800

    refactored /login and /register client routes

---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import LoginForm from "$lib/components/LoginForm.svelte";
+</script>
+
+<header class="p-3">
+  <h1 class="text-center">Login to continue</h1>
+</header>
+
+<main
+  class="flex flex-col border border-black items-center p-3 min-h-[500px] h-[80vh]"
+>
+  <div class="bg-slate-200 rounded-xl p-3 w-[450px]">
+    <LoginForm />
+    <div class="flex justify-center">
+      <a href="/auth/register">
+        <span class="text-neutral-500 hover:text-green-950"
+          >Forgot password?</span
+        >
+      </a>
+    </div>
+  </div>
+</main>
---
+++ frontend/src/routes/auth/register/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import RegisterForm from "$lib/components/RegisterForm.svelte";
+</script>
+
+<header class="p-3">
+  <h1 class="text-center">Register to start mixing!</h1>
+</header>
+
+<main
+  class="flex flex-col border border-black items-center p-3 min-h-[500px] h-[80vh]"
+>
+  <div class="bg-slate-200 rounded-xl p-3 w-[450px]">
+    <RegisterForm />
+    <div class="flex justify-center">
+      <a href="/auth/login">
+        <span class="text-neutral-500 hover:text-green-950"
+          >Already have an account? Log in!</span
+        >
+      </a>
+    </div>
+  </div>
+</main>
