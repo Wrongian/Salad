39547390b9fde7399031ae8a18c817fa9bc98aed
Linxin <prnis360@gmail.com>
2024-05-20 22:47:21 +0800

    added register query

--- frontend/src/scripts/queries.ts
+++ frontend/src/scripts/queries.ts
@@ -12,3 +12,17 @@ export const login = async (username: string, password: string): Promise<void> =
     });
     console.log(response)
 }
+
+export const register = async (email: string, username: string, password: string): Promise<void> => {
+
+    const response = await fetch(`${SERVER_IP_ADDR}/register`, {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify({
+            email: email,
+            username: username,
+            password: password
+        })
+    });
+
+}
