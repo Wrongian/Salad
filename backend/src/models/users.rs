ddd4788c3ffdab792b87814431697aa5224ca69e
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-22 03:21:33 +0800

    Create users.rs
    created user module

---
+++ backend/src/models/users.rs
@@ -0,0 +1,14 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct User {
+    pub username: String,
+    // hashed password
+    pub password: String,  
+    pub email: String,
+    pub bio: Option<String>,
+    pub is_private: bool,
+    pub salt: String,
+}
