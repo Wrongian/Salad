b0b71f0d8fdfca97c9076a7ccf2fffa8241702ca
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-22 02:02:06 +0800

    added schema

---
+++ backend/src/schema.rs
@@ -0,0 +1,30 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    _sqlx_migrations (version) {
+        version -> Int8,
+        description -> Text,
+        installed_on -> Timestamptz,
+        success -> Bool,
+        checksum -> Bytea,
+        execution_time -> Int8,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        #[max_length = 30]
+        username -> Varchar,
+        password -> Varchar,
+        email -> Varchar,
+        bio -> Nullable<Varchar>,
+        is_private -> Bool,
+        salt -> Varchar,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    _sqlx_migrations,
+    users,
+);
