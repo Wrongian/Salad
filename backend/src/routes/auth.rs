03542abf21d76fa1c319bf0ed87bdb2adb710b0b
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-24 00:58:54 +0800

    Update auth.rs
    Register and login function should return response

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -7,7 +7,8 @@ use scrypt::{
     Scrypt
 };
 use tide::prelude::*;
-use crate::db::{start_connection, user::get_user_id_from_name, user::get_password_from_id};
+use tide::Response;
+use crate::db::{start_connection, user::get_user_id_from_name, user::get_password_salt_from_id};
 use crate::db::user::create;
 use crate::models::users::User;
 
@@ -24,13 +25,53 @@ pub struct LoginParams {
     pub password : String,
 }
 
+#[derive(Debug, Serialize)]
+pub struct StandardBody {
+    pub result : bool,
+    pub err : String,
+}
+
 pub async fn login(mut req: Request<()>) -> tide::Result {
     let LoginParams {username, password} = req.body_json().await?;
     // probably need to validate this at some point
     let uid: i32 = req.session().get("user_id").expect("user_id not found in session");
-    
 
-    Ok(format!("Username: {}\n Password: {}", username, password).into())
+    // start a connection with the database
+    let mut conn = start_connection().await;
+
+    // get the password hash from db
+    let (password_hash, salt) = get_password_salt_from_id(&mut conn, uid).await;
+
+    let salt_string = SaltString::encode_b64(salt.as_bytes())?;
+    // verify password
+    if verify_password(&password, &salt_string, &password_hash) {
+        // password correct
+
+        // login the user
+        let user_id = get_user_id_from_name(&mut conn, &username).await;
+        
+        let session = req.session_mut();
+
+        // insert user_id into the session
+        session.insert("user_id", user_id)?;
+
+
+        return Ok(format!("result: {}\n err: {}", true, "Successfully logged in").into());
+    }
+    else {
+
+        // build response
+        let res_body = StandardBody {
+            result : false,
+            err : "Incorrect password".to_string(),
+        };
+        let mut response = Response::builder(400)
+            .body(tide::Body::from_form(&res_body)?)
+            .build();
+
+        return Ok(response)
+    }
+
 } 
 
 pub async fn register(mut req: Request<()>) -> tide::Result {
@@ -87,7 +128,7 @@ fn hash_password(password: &String, salt: &SaltString) -> String {
     }
 }
 
-fn verify_password(to_check: &String, salt: &SaltString, hash_string: &String) -> bool{
+fn verify_password(to_check: &String, salt: &SaltString, hash_string: &String) -> bool {
     let pass_arr = to_check.as_bytes();
     let res = Scrypt.hash_password(pass_arr, salt);
     match res {
