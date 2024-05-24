4d79c209480c33dbee7864bd322f56521ac6f151
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-24 21:51:24 +0800

    Update schema.rs

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,17 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    _sqlx_migrations (version) {
-        version -> Int8,
-        description -> Text,
-        installed_on -> Timestamptz,
-        success -> Bool,
-        checksum -> Bytea,
-        execution_time -> Int8,
-    }
-}
-
-diesel::table! {
     users (id) {
         id -> Int4,
         #[max_length = 30]
@@ -21,11 +10,6 @@ diesel::table! {
         bio -> Nullable<Varchar>,
         is_private -> Bool,
         salt -> Varchar,
-        display_name -> Varchar ,
+        display_name -> Varchar,
     }
 }
-
-diesel::allow_tables_to_appear_in_same_query!(
-    _sqlx_migrations,
-    users,
-);
