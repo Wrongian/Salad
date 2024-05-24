c94f4923e8e29dc123ed0db2cbd6008cdc3b3b52
Wrongian <higgsbosonsandquarks@gmail.com>
2024-05-25 01:52:07 +0800

    Added error store for error state management
    Co-authored-by: Linxin <CLnxn@users.noreply.github.com>

--- frontend/src/stores/stores.ts
+++ frontend/src/stores/stores.ts
@@ -1,3 +1,10 @@
-import { writable } from "svelte/store";
+import { writable, type Writable } from "svelte/store";
 
 export const authStore = writable(null)
+
+export const errorStore: Writable<TErrorContext> = writable()
+
+export type TErrorContext = {
+    addError: (message: string, statusCode: number) => void,
+    removeAt: (index: number) => void
+}
