d5ec57dedfe3a25246c1d9b2ad95b957adb9f443
Linxin <prnis360@gmail.com>
2024-05-26 18:17:55 +0800

    added request body and field validation for /login and /register
    Co-authored-by: Wrongian <90535821+Wrongian@users.noreply.github.com>

--- backend/src/routes/auth.rs
+++ backend/src/routes/auth.rs
@@ -1,43 +1,50 @@
+use crate::db::start_connection;
+use crate::db::user::{
+    check_user_exists, check_username_present, create, get_password_salt_from_id,
+    get_user_id_from_name,
+};
+use crate::models::users::User;
 use core::panic;
-use tide::{log::start, Request};
 use scrypt::{
     password_hash::{
-        rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, Salt, SaltString
+        rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, Salt, SaltString,
     },
-    Scrypt
+    Scrypt,
 };
 use tide::prelude::*;
 use tide::Response;
-use crate::db::user::{
-    check_user_exists, create, get_password_salt_from_id, get_user_id_from_name
-};
-use crate::db::start_connection;
-use crate::models::users::User;
+use tide::{log::start, Request};
+use validator::{Validate, ValidationError};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct RegisterParams {
-    pub email : String,
-    pub username : String,
-    pub password : String,
+    #[validate(email)]
+    pub email: String,
+    #[validate(length(max = 30))]
+    pub username: String,
+    #[validate(length(min = 5, max = 50))]
+    pub password: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct LoginParams {
-    pub username : String,
-    pub password : String,
+    #[validate(length(max = 30))]
+    pub username: String,
+    #[validate(length(min = 5, max = 50))]
+    pub password: String,
 }
 
 #[derive(Debug, Serialize)]
 pub struct StandardBody {
-    pub result : bool,
-    pub err : String,
+    pub result: bool,
+    pub err: String,
 }
 
-fn build_response(result: bool, err: String, status: u16) -> tide::Result{
+fn build_response(result: bool, err: String, status: u16) -> tide::Result {
     // build response
     let res_body = StandardBody {
-        result : result,
-        err : err,
+        result: result,
+        err: err,
     };
     let response = Response::builder(status)
         .body(tide::Body::from_json(&res_body)?)
@@ -46,11 +53,37 @@ fn build_response(result: bool, err: String, status: u16) -> tide::Result{
 }
 
 pub async fn login(mut req: Request<()>) -> tide::Result {
-    let LoginParams {username, password} = req.body_json().await?;
+    // process the body
+    let login_params: LoginParams;
+    match req.body_json().await {
+        Ok(params) => {
+            login_params = params;
+        }
+        Err(e) => {
+            return build_response(false, "Bad Request Body".to_string(), 400);
+        }
+    }
+    let username = &login_params.username;
+    let password = &login_params.password;
+
+    // validate login
+    match login_params.validate() {
+        Err(e) => {
+            // returns the validation error
+            return build_response(false, e.to_string(), 400);
+        }
+        _ => (),
+    }
 
     // start a connection with the database
     let mut conn = start_connection().await;
 
+    // check if the username is present in the database
+    if !(check_username_present(&mut conn, &username).await) {
+        return build_response(false, "User does not exist".to_string(), 400);
+    }
+
+    // get the user ID
     let uid = get_user_id_from_name(&mut conn, &username).await;
 
     // get the password hash from db
@@ -58,7 +91,7 @@ pub async fn login(mut req: Request<()>) -> tide::Result {
 
     let salt_string = SaltString::from_b64(&salt)?;
 
-    // check if already logged in 
+    // check if already logged in
     let is_logged_in: Option<i32> = req.session().get("user_id");
     if is_logged_in != None {
         return build_response(false, "Already Logged in".to_string(), 400);
@@ -70,23 +103,42 @@ pub async fn login(mut req: Request<()>) -> tide::Result {
 
         // login the user
         let user_id = get_user_id_from_name(&mut conn, &username).await;
-        
+
         let session = req.session_mut();
 
         // insert user_id into the session
         session.insert("user_id", user_id)?;
 
         return build_response(true, "".to_string(), 200);
-    }
-    else {
-
+    } else {
         return build_response(false, "Incorrect Password".to_string(), 400);
     }
-
-} 
+}
 
 pub async fn register(mut req: Request<()>) -> tide::Result {
-    let RegisterParams {email, username, password} = req.body_json().await?;
+    // process the body
+    let register_params: RegisterParams;
+    match req.body_json().await {
+        Ok(params) => {
+            register_params = params;
+        }
+        Err(e) => {
+            return build_response(false, "Bad Request Body".to_string(), 400);
+        }
+    }
+
+    let email = &register_params.email;
+    let username = &register_params.username;
+    let password = &register_params.password;
+
+    // validate register
+    match register_params.validate() {
+        Err(e) => {
+            // returns the validation error
+            return build_response(false, e.to_string(), 400);
+        }
+        _ => (),
+    }
     // generate salt
     let salt: SaltString = generate_salt();
     // hash salt
@@ -95,7 +147,7 @@ pub async fn register(mut req: Request<()>) -> tide::Result {
     let salt_str = salt.to_string();
     // get back the SaltString
     // let salt = SaltString::from_b64(salt_str);
-    
+
     // create new user
     let new_user = User {
         username: username.clone(),
@@ -106,10 +158,10 @@ pub async fn register(mut req: Request<()>) -> tide::Result {
         salt: salt_str,
         display_name: username.clone(),
     };
-    
+
     // start a connection with the database
     let mut conn = start_connection().await;
-    
+
     // check if the user already exists
     if check_user_exists(&mut conn, &username, &email).await {
         return build_response(true, "Username or Email already taken".to_string(), 400);
@@ -118,7 +170,7 @@ pub async fn register(mut req: Request<()>) -> tide::Result {
     // create user
     let user = create(&mut conn, &new_user).await;
     let user_id = get_user_id_from_name(&mut conn, &username).await;
-    
+
     // log the user in
     let session = req.session_mut();
 
@@ -151,17 +203,16 @@ fn verify_password(to_check: &String, salt: &SaltString, hash_string: &String) -
     let res = Scrypt.hash_password(pass_arr, salt);
     match res {
         Ok(hash) => {
-
             println!("PASSWORD:");
-            println!("{}",hash.to_string());
-            println!("{}",hash_string);
+            println!("{}", hash.to_string());
+            println!("{}", hash_string);
             if hash.to_string() == *hash_string {
                 return true;
             }
             return false;
         }
         Err(e) => {
-            println!("Error: {}",e.to_string());
+            println!("Error: {}", e.to_string());
             return false;
         }
     }
