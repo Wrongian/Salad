12221acb828e7894e8a4ff767f13798aad47bb72
Linxin <prnis360@gmail.com>
2024-06-02 15:09:03 +0800

    updated error flash ui
    Co-authored-by: Wrongian <90535821+Wrongian@users.noreply.github.com>

--- frontend/src/lib/components/ErrorFlashCard.svelte
+++ frontend/src/lib/components/ErrorFlashCard.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import close from "$lib/assets/close.png";
-  import { onMount } from "svelte";
   import { fade, fly } from "svelte/transition";
 
   export let message: string = "Something really bad has occurred.";
@@ -10,16 +9,18 @@
 
 <div
   in:fly={{ y: 200, duration: 500 }}
-  out:fade={{ duration: 500 }}
-  class="w-[450px] bg-red-100 border border-red-600 text-red-600 px-2 py-3 rounded relative
+  out:fade={{ duration: 300 }}
+  class="flex w-[450px] bg-red-100 border border-red-600 text-red-600 px-2 py-3 rounded relative
   overflow-y-hidden max-w-[350px] max-h-[200px]"
 >
-  <strong class="font-bold">Oh No!</strong>
-  <span class="inline-block">
-    {message}
-  </span>
+  <div class="flex flex-1 flex-col">
+    <strong class="block font-bold">Oh No!</strong>
+    <span class="block">
+      {message}
+    </span>
+  </div>
   <button
-    class="p-0 ml-[90px] mr-0 my-0 bg-inherit hover:bg-inherit"
+    class="block p-0 ml-[90px] mr-0 my-0 bg-inherit hover:bg-inherit"
     on:click={onClose}
   >
     <img src={close} alt="X" class="bg-inherit hover:bg-inherit h-3 w-3" />
