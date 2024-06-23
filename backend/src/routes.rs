66d4d3e3d5b06b8abd39418aa5eef59eda006f81
Linxin <prnis360@gmail.com>
2024-06-23 22:50:31 +0800

    nit: add links controller

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -1,2 +1,3 @@
 pub mod auth;
+pub mod links_controller;
 pub mod profile_controller;
---
+++ backend/src/routes/links_controller.rs
@@ -0,0 +1,5 @@
+use crate::TideState;
+use std::sync::Arc;
+use tide::Request;
+
+pub async fn add_link(req: Request<Arc<TideState>>) {}
