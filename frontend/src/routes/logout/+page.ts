b888885f853986daace8072aa4a724fd0d53dddb
Wrongian <higgsbosonsandquarks@gmail.com>
2024-06-25 00:43:56 +0800

    Removed Redundant Check

--- frontend/src/routes/logout/+page.ts
+++ frontend/src/routes/logout/+page.ts
@@ -10,9 +10,7 @@ import { logout } from "$lib/scripts/queries";
 export const load: PageLoad = async ({fetch, url}) => {
     let next = "";
     const params = url.searchParams;
-    if (params.has("next")) {
-        next = params.get("next") || "";
-    }
+    next = params.get("next") || "";
 
     await logout(fetch,next);
     
