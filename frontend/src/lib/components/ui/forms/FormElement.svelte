fa975a5e408d0f5749eafa76f6a344ee6e459406
Wrongian <higgsbosonsandquarks@gmail.com>
2024-06-29 01:17:25 +0800

    Added Form Components

---
+++ frontend/src/lib/components/ui/forms/FormElement.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    // later change to id
+    export let name = "";
+    export let text = "";
+</script>
+
+<div class="relative group mb-2 py-4">
+    <input type="text" id="form-element-{name}" class="block font-medium px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600" placeholder=""/>
+</div>
