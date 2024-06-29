2ca508ba6954114aea1d998bb67d48ec6a13ce15
Wrongian <higgsbosonsandquarks@gmail.com>
2024-06-30 04:22:36 +0800

    Implemented edit link functionality
    Co-authored-by: Linxin <81039802+CLnxn@users.noreply.github.com>

--- frontend/src/lib/components/ui/links/Link.svelte
+++ frontend/src/lib/components/ui/links/Link.svelte
@@ -2,58 +2,87 @@
     import type { ListData } from "$lib/types/Profile";
     import type { TLink } from "$lib/scripts/response-validator";
     import * as Avatar from "$lib/components/ui/avatar/index.js";
-    import type { Callback } from "$lib/types/Callback";
-    export let modalCallback : Callback;
+    import type { ModalCallback } from "$lib/types/Callback";
+    import { updateLinkBio, updateLinkHref, updateLinkTitle, deleteLink } from "$lib/scripts/queries";
+    import { invalidateAll } from "$app/navigation";
+    import { Trash2 } from "lucide-svelte";
+    export let modalCallback : ModalCallback;
+
     export let listData : ListData;
     let link: TLink;
     let isFocused = false;
+  
     $: link = listData.linkData;
     $: dragClass = listData.isDragged ? "opacity-0" : "";
-
-    // placeholder for now
-    const submitLinkName = (id : number) => {
+    const submitLinkName = async (id : number) => {
       isFocused = false;
+      await updateLinkTitle({
+          title: link.title ?? ""
+      }, id);
+      await invalidateAll();
     } 
-    // placeholder for now
-    const submitDescription = (id : number) => {
+
+    const submitDescription = async (id : number) => {
       isFocused = false;
+      await updateLinkBio({
+        bio: link.description ?? "" 
+      }, id);
+      await invalidateAll();
     }
-    // placeholder for now
-    const submitURL = (id : number) => {
+
+    const submitURL = async (id : number) => {
       isFocused = false;
+      await updateLinkHref({
+        href: link.href ?? ""
+      }, id);
+      await invalidateAll();
+    }
+
+    const deleteLinkCallback = async (id: number) => {
+      await deleteLink(id);
+      await invalidateAll();
     }
 </script>
 
-  <div draggable={!isFocused} class="w-[1350px] rounded overflow-hidden mx-auto my-2 border-solid border-4 border-lime-300">
-    <div class="flex shadow-lg p-3">
+  <div draggable={!isFocused} class="w-[90vw] rounded overflow-hidden mx-auto my-2 border-solid border-4 border-lime-300 {dragClass}">
+    <div class="relative flex shadow-lg p-3">
+        <button class="absolute button top-0 right-0 w-[25px] h-[25px] mr-2 mt-2 hover:opacity-100 opacity-40"
+          on:click={() => deleteLinkCallback(link.id)}>
+            <Trash2 />
+        </button>
+        <div>
+          {link.id}
+          next id: {link.next_id}
+        </div>
         <div class="flex flex-col items-center min-w-[100px] justify-center mr-4">
             <div class="px-4 pt-2 relative">
-        <button  on:click={() => {modalCallback()}} >
+        <button  on:click={() => {modalCallback(link.id)}} >
         <Avatar.Root class="w-[100px] h-[100px] ring-2 ring-lime-300 ">
-            <Avatar.Image class="z-4 hover:brightness-50 peer-hover/image" src={link.href} alt="">
+            <Avatar.Image class="z-4 hover:brightness-50 peer-hover/image" src={link.img_src} alt="">
             </Avatar.Image>
             <Avatar.Fallback></Avatar.Fallback>
         </Avatar.Root>
         </button> 
-            <!-- <span class="z-0 select-none invisible peer-hover:visible absolute mx-12 my-6 left-0 w-1/4 text-wrap text-center text-m text-medium text-white">
-            Edit Image
-            </span> -->
-         </div>
+        
+        </div>
             <div class="text-center flex gap-1 flex-wrap justify-center">
             </div>
         </div>
         <div class="lg:w-4/5 md:w-3/4">
-            <div class="mb-1 py-1 w-[1100px]">
+            <div class="mb-1 py-1 w-[4/5] w-full">
                 <label for="change-name-{link.id}" class="block mb-2 font-medium text-xl text-gray-900">Change Link Name:</label>
                 <input on:focus={() => {isFocused = true}} on:focusout={() => {submitLinkName(link.id)}} type="text" id="change-name-{link.id}" bind:value={link.title} class="block font-medium px-0 w-full text-2xl text-gray-900 border-lime-300 bg-transparent border-b-2 focus:outline-none focus:ring-0 focus:border-lime-600" placeholder="" />
-            </div>
-            <div class="mb-1 py-1 w-[1100px]">
-                <!--Might need validation for URL -->
+            </div> 
+            
+            <div class="mb-1 py-1 w-full">
                 <label for="change-url-{link.id}" class="block mb-2 font-medium text-xl text-gray-900">Change Link URL:</label>
                 <input on:focus={() => {isFocused = true}} on:focusout={() => {submitURL(link.id)}} type="text" id="change-url-{link.id}" bind:value={link.href} class="block font-medium px-0 w-full text-2xl text-gray-900 border-lime-300 bg-transparent border-b-2 focus:outline-none focus:ring-0 focus:border-lime-600" placeholder="" />
             </div>
-            <label for="change-description-{link.id}" class="block mb-2 font-medium text-xl text-gray-900">Change Description:</label>
-            <textarea on:focus={() => {isFocused = true}} on:focusout={() => {submitDescription(link.id)}} bind:value={link.description} id="change-description-{link.id}" class="block w-[1100px] h-[100px] py-2 px-2 "/>
-          </div>
+            <div class="w-full">
+              <label for="change-description-{link.id}" class="block mb-2 font-medium text-xl text-gray-900">Change Description:</label>
+              <textarea on:focus={() => {isFocused = true}} on:focusout={() => {submitDescription(link.id)}} bind:value={link.description} id="change-description-{link.id}" class="block rounded-lg ring-2 ring-lime-300 w-full h-[100px] py-2 px-2 "/>
+            </div>
+            
+        </div>
     </div>
 </div>
