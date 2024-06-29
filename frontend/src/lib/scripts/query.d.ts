e53720aee2455cba59864cb94a2612f9957f1a92
Wrongian <higgsbosonsandquarks@gmail.com>
2024-06-30 04:20:41 +0800

    Added remaining profile and query routes
    Co-authored-by: Linxin <81039802+CLnxn@users.noreply.github.com>

--- frontend/src/lib/scripts/query.d.ts
+++ frontend/src/lib/scripts/query.d.ts
@@ -21,6 +21,23 @@ export type TCreateLinkPayload = {
   href: string
 }
 
+export type TUpdateLinkTitlePayload = {
+  title: string
+}
+
+export type TUpdateLinkBioPayload = {
+  bio: string
+}
+
+export type TUpdateLinkHrefPayload = {
+  href: string
+}
+
+export type TReorderPayload = {
+  link_id: number,
+  new_position_id: number | null 
+}
+
 export type TResponseBody = {
   result: boolean;
   err: string;
