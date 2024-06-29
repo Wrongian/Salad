2d96f52d93a97e84f25a0d07fcb9a772673826e6
Wrongian <higgsbosonsandquarks@gmail.com>
2024-06-30 04:19:27 +0800

    Updated Link Validation

--- frontend/src/lib/scripts/response-validator.ts
+++ frontend/src/lib/scripts/response-validator.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 import type { TResponseBody } from "./query";
 
-export const authResponseValidator = (res: any): res is TResponseBody => {
+export const standardResponseValidator = (res: any): res is TResponseBody => {
   if (!("result" in res) || !("err" in res)) return false;
   return typeof res.result === "boolean" && typeof res.err === "string";
 };
@@ -27,11 +27,11 @@ export const TProfileBodyValidator = Joi.object<TProfileBody>({
 export type TLink = {
   id: number;
   user_id: number;
-  next_id: number | undefined
-  title: string | undefined;
+  next_id: number | null 
+  title: string | null;
   href: string;
-  description: string | undefined;
-  img_src: string | undefined;
+  description: string | null;
+  img_src: string | null;
 }
 
 export const TLinkBodyValidator = Joi.object<{links: TLink[]}>({
@@ -39,11 +39,11 @@ export const TLinkBodyValidator = Joi.object<{links: TLink[]}>({
           Joi.object({
             id: Joi.number(),
             user_id: Joi.number().required(),
-            next_id: Joi.number().optional(),
+            next_id: Joi.number().allow(null).optional(),
             href: Joi.string().required(),
-            title: Joi.string().optional(),
-            description: Joi.string().optional(),
-            img_src: Joi.string().optional(),
+            title: Joi.string().allow(null).optional(),
+            description: Joi.string().allow(null).optional(),
+            img_src: Joi.string().allow(null).optional(),
           })
         ).min(0)
 });
