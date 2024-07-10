e8f02c17add01dfb1713c197c16a3ea096f104de
Linxin <prnis360@gmail.com>
2024-07-10 22:12:11 +0800

    Reformat fetch.ts
    Co-authored-by: Wrongian <90535821+Wrongian@users.noreply.github.com>

--- frontend/src/lib/scripts/fetch.ts
+++ frontend/src/lib/scripts/fetch.ts
@@ -3,80 +3,95 @@ const BLACKSWAN_ERROR_STATUS_CODE = 500;
 const BAD_REQUEST_STATUS = 400;
 import { validatePayload, type Validator } from "./validator.js";
 import { TErrorValidator, type TError } from "./validation/error.js";
-import { TStandardPayloadValidator, type TStandardPayload } from "./validation/response.js";
+import {
+  TStandardPayloadValidator,
+  type TStandardPayload,
+} from "./validation/response.js";
 import { addError } from "$lib/modules/Errors.svelte";
-
 
 const MASKED_ERROR_MESSAGE =
   "Oh no! Looks like something went wrong. Please try again later.";
 
-type HttpMethods = "GET" | "POST" | "PUT" | "PATCH" | "UPDATE" | "DELETE" | "HEAD";
+type HttpMethods =
+  | "GET"
+  | "POST"
+  | "PUT"
+  | "PATCH"
+  | "UPDATE"
+  | "DELETE"
+  | "HEAD";
 type fetch = typeof fetch;
 
-export async function validateFetch<T, U extends object>(endpoint: string, method: HttpMethods, payload: U, validator: Validator<T>): Promise<T | null> {
-  const hasBody = method == "GET" || method == "DELETE" || method == "HEAD"
+export async function validateFetch<T, U extends object>(
+  endpoint: string,
+  method: HttpMethods,
+  payload: U,
+  validator: Validator<T>,
+): Promise<T | null> {
+  const hasBody = method == "GET" || method == "DELETE" || method == "HEAD";
   // check body
-  const request = hasBody ? {
-    method,
-    body: JSON.stringify(payload)
-  } : {
-    method
-  };
+  const request = hasBody
+    ? {
+        method,
+        body: JSON.stringify(payload),
+      }
+    : {
+        method,
+      };
 
   // get the response
   const response = await fetch(endpoint, request);
 
-  //  
-  const jsonBody = await response.json().catch(_ => {
+  //
+  const jsonBody = await response.json().catch((_) => {
     // black swan
     blackSwanError.set({
       status: BLACKSWAN_ERROR_STATUS_CODE,
       message: MASKED_ERROR_MESSAGE,
     });
     return null;
-  })
+  });
 
   if (!response.ok) {
     return await validatePayload<TError>(jsonBody, TErrorValidator)
       .then(({ err }) => {
         if (response.status === 400) {
-          addError(err, response.status)
+          addError(err, response.status);
         } else if (response.status === 403) {
           // forbidden page
         } else if (response.status === 404) {
-          // not found 
+          // not found
         } else {
           blackSwanError.set({
             status: BLACKSWAN_ERROR_STATUS_CODE,
             message: MASKED_ERROR_MESSAGE,
           });
         }
-        return null
+        return null;
       })
-      .catch(_ => {
+      .catch((_) => {
         blackSwanError.set({
           status: BLACKSWAN_ERROR_STATUS_CODE,
           message: MASKED_ERROR_MESSAGE,
         });
-        return null
-      })
-
+        return null;
+      });
   }
   // { payload: T }
   if ("payload" in jsonBody) {
     // validate
-    return await validatePayload(jsonBody.payload, validator).catch(_ => {
+    return await validatePayload(jsonBody.payload, validator).catch((_) => {
       blackSwanError.set({
         status: BLACKSWAN_ERROR_STATUS_CODE,
         message: MASKED_ERROR_MESSAGE,
       });
-      return null
-    })
+      return null;
+    });
   } else {
     blackSwanError.set({
       status: BLACKSWAN_ERROR_STATUS_CODE,
       message: MASKED_ERROR_MESSAGE,
     });
-    return null
+    return null;
   }
 }
