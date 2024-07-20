a05b90a71a3195ef8ade80a5685d30b9f06280cd
Linxin <prnis360@gmail.com>
2024-07-20 17:47:07 +0800

    Change field visibility

--- backend/src/models/follows.rs
+++ backend/src/models/follows.rs
@@ -4,14 +4,14 @@ use diesel::{deserialize::Queryable, prelude::Insertable};
 #[diesel(table_name = crate::schema::follows)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct InsertFollow {
-    from_id: i32,
-    to_id: i32,
+    pub from_id: i32,
+    pub to_id: i32,
 }
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::pending_follow_requests)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct InsertFollowRequest {
-    from_id: i32,
-    to_id: i32,
+    pub from_id: i32,
+    pub to_id: i32,
 }
