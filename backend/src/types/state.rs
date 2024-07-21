d8b9ef3c89073c6831870fe5063a35f54dcb6b38
Wrongian <higgsbosonsandquarks@gmail.com>
2024-07-22 03:19:27 +0800

    Added generic typing for state

--- backend/src/types/state.rs
+++ backend/src/types/state.rs
@@ -1,4 +1,5 @@
 use crate::connectors::smtp::email::EmailService;
+use crate::connectors::smtp::smtp_service::SMTPService;
 use aws_sdk_s3::{self as s3, config};
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
@@ -11,16 +12,16 @@ use tempfile::TempDir;
 pub type TidePool = Pool<ConnectionManager<PgConnection>>;
 
 // this is actual state of the tide app as a struct
-pub struct TideState {
+pub struct TideState<T: SMTPService> {
     pub tide_pool: TidePool,
     pub s3_client: s3::Client,
     pub tempdir: TempDir,
     // might want to make this a dynamic type in the future
-    pub email_service: EmailService,
+    pub email_service: T,
 }
 
 // this returns the path of the directory
-impl TideState {
+impl<T: SMTPService> TideState<T> {
     fn path(&self) -> &Path {
         self.tempdir.path()
     }
