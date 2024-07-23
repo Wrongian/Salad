18660a857bd9cd59b9bd5b652299adbfa3bfee29
Wrongian <higgsbosonsandquarks@gmail.com>
2024-07-24 03:46:55 +0800

    Updated schema

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -50,4 +50,9 @@ diesel::joinable!(images -> users (user_id));
 diesel::joinable!(links -> users (user_id));
 diesel::joinable!(reset_password_request -> users (user_id));
 
-diesel::allow_tables_to_appear_in_same_query!(images, links, reset_password_request, users,);
+diesel::allow_tables_to_appear_in_same_query!(
+    images,
+    links,
+    reset_password_request,
+    users,
+);
