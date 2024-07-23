6b625652a5d3330c263537b747513086486fcfc6
Wrongian <higgsbosonsandquarks@gmail.com>
2024-07-24 00:42:17 +0800

    Added mock_connection as a connector in mod.rs

--- backend/src/connectors/db/mod.rs
+++ backend/src/connectors/db/mod.rs
@@ -3,3 +3,16 @@ pub mod image;
 pub mod link;
 pub mod reset;
 pub mod user;
+
+use diesel::{Connection, PgConnection};
+use dotenvy::dotenv;
+use std::env;
+
+pub async fn mock_connection() -> PgConnection {
+    dotenv().expect("No .env file found");
+
+    let database_url = env::var("DATABASE_URL").expect("No database url found");
+
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
