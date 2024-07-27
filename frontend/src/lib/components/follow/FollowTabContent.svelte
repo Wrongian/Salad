3f5a56f574ecdbb684cee49a795e0028d9318c33
Linxin <prnis360@gmail.com>
2024-07-27 18:29:56 +0800

    Refactor search follow tab into svelte component

---
+++ frontend/src/lib/components/follow/FollowTabContent.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { EllipsisVertical } from "lucide-svelte";
+  import * as Avatar from "../ui/avatar";
+  import * as Pagination from "../ui/pagination";
+  import type { TPaginatedProfile } from "$lib/scripts/validation/response";
+
+  export let paginatedFollowRecords: TPaginatedProfile[];
+  export let totalRecords: number;
+  export let recordsPerPage: number;
+  export let currentPageIndex: number;
+</script>
+
+<div>
+  {#each paginatedFollowRecords as { username, img_src, display_name }, i}
+    <div
+      class="h-20 p-8 flex gap-x-4 items-center justify-between shadow-sm rounded-xl border border-gray-100 mb-2"
+    >
+      <Avatar.Root class="w-10 h-10 ring-2 ring-lime-300">
+        <Avatar.Image src={img_src} alt="" />
+        <Avatar.Fallback></Avatar.Fallback>
+      </Avatar.Root>
+      <div class="flex-1">
+        <a
+          href={`../profiles/${username}`}
+          data-sveltekit-preload-data="tap"
+          class="text-left select-none cursor-pointer"
+        >
+          {display_name}
+        </a>
+      </div>
+
+      <button class="m-0 p-0">
+        <EllipsisVertical />
+      </button>
+    </div>
+  {/each}
+</div>
+<div class="absolute bottom-0 flex justify-center w-full">
+  <Pagination.Root
+    count={totalRecords}
+    perPage={recordsPerPage}
+    bind:page={currentPageIndex}
+    let:pages
+    let:currentPage
+  >
+    <Pagination.Content>
+      <Pagination.Item>
+        <Pagination.PrevButton />
+      </Pagination.Item>
+      {#each pages as page (page.key)}
+        {#if page.type === "ellipsis"}
+          <Pagination.Item>
+            <Pagination.Ellipsis />
+          </Pagination.Item>
+        {:else}
+          <Pagination.Item>
+            <Pagination.Link {page} isActive={currentPage == page.value}>
+              {page.value}
+            </Pagination.Link>
+          </Pagination.Item>
+        {/if}
+      {/each}
+      <Pagination.Item>
+        <Pagination.NextButton />
+      </Pagination.Item>
+    </Pagination.Content>
+  </Pagination.Root>
+</div>
