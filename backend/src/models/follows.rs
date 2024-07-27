6de4983d89e9166f7bbd50b064d4711e5c71e3a6
Linxin <prnis360@gmail.com>
2024-07-28 01:16:55 +0800

    Add paginated pending follow connectors

--- backend/src/models/follows.rs
+++ backend/src/models/follows.rs
@@ -1,4 +1,4 @@
-use diesel::{deserialize::Queryable, prelude::Insertable};
+use diesel::{deserialize::Queryable, prelude::Insertable, Selectable};
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::follows)]
@@ -12,6 +12,13 @@ pub struct InsertFollow {
 #[diesel(table_name = crate::schema::pending_follow_requests)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct InsertFollowRequest {
+    pub from_id: i32,
+    pub to_id: i32,
+}
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::pending_follow_requests)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct GetFollowRequest {
     pub from_id: i32,
     pub to_id: i32,
 }
