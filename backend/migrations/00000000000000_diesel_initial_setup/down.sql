819468aac930a14b389a93d2fde5393135993ade
Linxin <prnis360@gmail.com>
2024-07-30 23:53:24 +0800

    Add insights migration

--- backend/migrations/00000000000000_diesel_initial_setup/down.sql
+++ backend/migrations/00000000000000_diesel_initial_setup/down.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS pending_follow_requests;
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS links;
 DROP TABLE IF EXISTS reset_password_request;
+DROP TABLE IF EXISTS user_insights;
 DROP TABLE IF EXISTS users;
 DROP FUNCTION IF EXISTS diesel_manage_updated_at(_tbl regclass);
 DROP FUNCTION IF EXISTS diesel_set_updated_at();
