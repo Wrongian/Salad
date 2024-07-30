b817294d0f30185be54a5e0aaf7189df33298420
Wrongian <higgsbosonsandquarks@gmail.com>
2024-07-31 06:00:01 +0800

    Added routes to main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -27,12 +27,15 @@ use saladify::routes::links::get::get_links;
 use saladify::routes::links::update::{
     reorder_links, update_link_bio, update_link_href, update_link_picture, update_link_title,
 };
+use saladify::routes::notifications::{
+    delete::delete_all_notifications, get::get_notifications, update::read_notification,
+};
 use saladify::routes::profiles::get::{get_profile, get_username};
 use saladify::routes::profiles::update::{update_display_profile, update_profile_image};
+use saladify::routes::search::get::search_users;
 use saladify::routes::settings::settings::{
     change_email, change_password, change_username, update_privacy,
 };
-use saladify::routes::search::get::search_users;
 use saladify::types::state::TideState;
 use std::env;
 use std::sync::Arc;
@@ -161,13 +164,18 @@ async fn main() -> tide::Result<()> {
     app.at("/get-email").post(get_email);
     app.at("/password-code").post(check_password_code);
     app.at("/reset-password").post(reset_password);
-  
+
     // settings
     app.at("/change-username").post(change_username);
     app.at("/change-password").post(change_password);
     app.at("/change-email").post(change_email);
     app.at("/update-privacy").post(update_privacy);
-  
+
+    // notifications
+    app.at("/notifications").delete(delete_all_notifications);
+    app.at("/notifications").get(get_notifications);
+    app.at("/notifications").put(read_notification);
+
     // search
     app.at("/search").get(search_users);
 
