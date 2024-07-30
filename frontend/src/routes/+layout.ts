81fa3ca16c8183be8630c7f908764e74e743d1e3
Wrongian <higgsbosonsandquarks@gmail.com>
2024-07-31 06:02:55 +0800

    Added notifications to the outermost layout for the navbar

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,13 +1,23 @@
-import { getIsLoggedIn, getUsername } from "$lib/scripts/queries";
+import {
+  getIsLoggedIn,
+  getNotifications,
+  getUsername,
+} from "$lib/scripts/queries";
+import type { TNotificationsPayload } from "$lib/scripts/validation/response";
 
 // disable server-side rendering
 // export const ssr = false;
 export const load = async ({ fetch }) => {
   const isLoggedIn = await getIsLoggedIn(fetch);
   var username = await getUsername(fetch);
+  var notifications: TNotificationsPayload = { notifications: [] };
+  if (username) {
+    notifications = await getNotifications(fetch);
+  }
 
   return {
     isLoggedIn: isLoggedIn,
     username: username,
+    notifications: notifications,
   };
 };
